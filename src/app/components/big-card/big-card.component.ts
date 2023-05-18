78010f00457f1d8eb23a262010d0243551b9f2a8
Draggner <davisamueldragg@gmail.com>
2023-05-18 15:22:18 -0300

    Adicionando propriedades dinâmicas nos componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+  // @Input()
+  // photoCover: string = "../../../assets/1683561061810.jpg"
+  // @Input()
+  // cardTitle: string = "Nova atualização do Angular"
+  // @Input()
+  // cardDescription: string = "O framework Angular nos ultimos dias sofreu modificações e uma nova atualização do Angular foi estabelecida, com novas funcionalidades e tendências de uso para os desenvolvedores Angular. Diante a esse fato..."
 
+  @Input()
+  photoCover: string = "../../../assets/1683561061810.jpg"
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = ""
 }
