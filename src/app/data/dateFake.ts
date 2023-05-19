5a19ecf61cf9293834affd16a41c715e1f8ca13a
Draggner <davisamueldragg@gmail.com>
2023-05-18 22:29:23 -0300

    Finalização do Projeto

--- src/app/data/dateFake.ts
+++ src/app/data/dateFake.ts
@@ -2,7 +2,43 @@ export const DataFake = [
   {
     "id": "1",
     "photo": '../../../assets/1683561061810.jpg',
-    "title": 'NOVO ANGULAR DEVELOPER',
-    "description": "A nova atualização e anunciamento do Framework Angular, os desevolvedores começaram a utilizar e esta sendo uma nova tendência de mercado."
+    "title": 'O Conclave das Bruxas: Pactos Sombrios e Rituais Antigos',
+    "description": "Adentre um mundo onde bruxas poderosas se reúnem em um misterioso conclave, realizando pactos sombrios e conduzindo rituais antigos. Descubra os segredos ocultos que envolvem a busca por poder além da compreensão humana. Nesse cenário macabro, as bruxas se empenham em obter conhecimentos e habilidades sobrenaturais, enfrentando desafios e perigos inimagináveis para alcançar seus objetivos sombrios."
   },
+  {
+    "id": "2",
+    "photo": '../../../assets/1683561061810.jpg',
+    "title": 'O Domínio das Sacerdotisas: Guardiãs da Magia Ancestral',
+    "description": "Explore um mundo onde sacerdotisas se destacam como guardiãs da magia ancestral. Elas possuem conhecimentos místicos transmitidos ao longo de gerações, protegendo e preservando segredos ocultos. Essas mulheres poderosas dominam rituais sagrados, utilizam artefatos antigos e são capazes de canalizar energias místicas para cura, proteção e até mesmo para fins mais sinistros. Descubra como as sacerdotisas equilibram a dualidade entre luz e sombra em seu papel como guardiãs da magia."
+  },
+  {
+    "id": "3",
+    "photo": '../../../assets/1683561061810.jpg',
+    "title": 'O Legado dos Feiticeiros: Feitiçaria Proibida e Poder Desenfreado',
+    "description": "Entre em um mundo onde feiticeiros herdam um legado ancestral de feitiçaria proibida. Esses seres dominam magias antigas e realizam rituais ocultos que desafiam as leis da natureza. Com acesso a conhecimentos sombrios e poderes desenfreados, os feiticeiros são capazes de manipular elementos, invocar criaturas sobrenaturais e até mesmo alterar a realidade ao seu bel-prazer. Contudo, sua busca pelo poder ilimitado pode ter consequências nefastas."
+  },
+  {
+    "id": "4",
+    "photo": '../../../assets/1683561061810.jpg',
+    "title": 'Além do Véu: Banshees e os Lamentos dos Condenados',
+    "description": "Explore um mundo além do véu, onde as banshees, entidades sobrenaturais, vagam entre os vivos. Essas criaturas sombrias são reconhecidas por seus lamentos angustiantes, que ecoam pelas noites silenciosas. Diz a lenda que as banshees anunciam a morte iminente de alguém. Descubra os segredos por trás dessas entidades e testemunhe os encontros aterradores entre os vivos e os condenados ao infortúnio eterno."
+  },
+  {
+    "id": "5",
+    "photo": '../../../assets/1683561061810.jpg',
+    "title": 'Reino das Trevas: Demônios e a Ascensão do Mal',
+    "description": "Adentre o Reino das Trevas, onde demônios de diversas categorias emergem e desencadeiam a ascensão do mal. Essas entidades infernais, sedentas por poder e destruição, buscam espalhar caos e desespero pelo mundo. Testemunhe batalhas épicas entre heróis corajosos e demônios implacáveis. Descubra os segredos da invocação demoníaca e as consequências catastróficas que acompanham aqueles que ousam brincar com forças além da compreensão humana."
+  },
+  {
+    "id": "6",
+    "photo": '../../../assets/1683561061810.jpg',
+    "title": 'Espíritos Malévolos: Encontros com o Sobrenatural',
+    "description": "Aventure-se em um mundo repleto de espíritos malévolos, entidades sobrenaturais que assombram os vivos com sua presença sinistra. Esses espíritos vingativos são motivados por mágoas do passado e estão determinados a assombrar e atormentar os vivos. Conheça histórias assustadoras de encontros com esses seres e descubra as técnicas de proteção e exorcismo utilizadas para lidar com o sobrenatural."
+  },
+  {
+    "id": "7",
+    "photo": '../../../assets/1683561061810.jpg',
+    "title": 'Culturas Perdidas: Mistérios e Ritos Ocultos',
+    "description": "Desvende os segredos das culturas perdidas que habitaram tempos remotos. Explore ruínas antigas e descubra os mistérios ocultos por trás de suas civilizações. Mergulhe nos ritos sagrados que transcendem o tempo e testemunhe as práticas místicas que influenciaram o curso da história. Essas culturas ancestrais deixaram marcas profundas na humanidade, e desvendar seus segredos é uma jornada fascinante em busca do conhecimento perdido."
+  }
 ]
